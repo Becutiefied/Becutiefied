3d515d1079e16aa8735cbc27305e0c6243aa76b2
Becutiefied <168904430+Becutiefied@users.noreply.github.com>
2024-08-25 14:28:53 +0200

    Add files via upload
    Dice app

---
+++ ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+      
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        var score = 0
+        
+        //.ramdomelement do the same > as Int.random
+        
+        diceImageView1.image = diceArray[Int.random(in: 1...5)]
+        diceImageView2.image = diceArray[Int.random(in: 1...5)]
+        
+        score += 1
+        
+    }
+    
+}
